789d5aad1689cfd125aacf6eb6b77be481847bff
Ali Lutfi <ali.lutfi@mynbps.org>
2020-12-15 20:33:50 +0000

    Started the project, will build more upon this.

--- App.java
+++ App.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("(The game has begun, and astronaut Dawson meets you at the space station.");
+        System.out.println("Greetings, fellow astronaut.");
+        System.out.println("Would you like to hop in my spaceship with me and go on a little..");
+        System.out.println("Diamond-meteorite hunting trip!?!?!");
+        while(True){
+          System.out.println("(For the purpose of the game, type 'Yes' with a capitalized Y, or 'No' with a capitalized N, depending on your interests :D");
+          
+        }
+
+
     }
 }
