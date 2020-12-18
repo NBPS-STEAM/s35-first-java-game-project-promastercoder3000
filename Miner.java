c5d2f16f4cd50c2cd18a4e5348587211915c0caf
Ali Lutfi <ali.lutfi@mynbps.org>
2020-12-18 20:13:52 +0000

    added more to the miner class

--- Miner.java
+++ Miner.java
@@ -1,7 +1,8 @@
 public class Miner{
   double wealth;
   int health;
-  double randomNum;
+  String materials = {"Bronze", "Gold", "Silver", "Diamond"};
+  String inventory = {};
   
   public Player(int playerHealth, double playerWealth){
     health = playerHealth;
@@ -13,8 +14,29 @@ public class Miner{
     System.out.println("You have taken 10 damage. You are now at " + health + " health!");
   }
 
-  public double increaseWealth(randomNum){
-      wealth = wealth + randomNum;
-      System.out.println("You now have " + wealth + " dollars! Nice!!!!!");
+ 
+  public void addMaterialAndIncreaseWealth(){
+      int randomNum = (int)(Math.random() * 4);
+      randMaterial = materials.get(randomNum);
+      inventory.add(randMaterial);
+      if (randMaterial.equals(materials.get(0))){
+        wealth += 1.25;
+      }
+      else if (randMaterial.equals(materials.get(1))){
+        wealth += 1.50;
+      }
+      else if (randMaterial.equals(materials.get(2))){
+        wealth += 1.75;
+      }
+      else {
+        wealth += 2.00;
+      }
+      System.out.println("You have acquired: " + randMaterial);
+      System.out.println("Your current net worth is: " + wealth);
   }
+
+  public void checkRequirementsToWin(){
+    
+  }
+
 }
