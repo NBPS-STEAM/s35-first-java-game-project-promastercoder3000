137fdfbb279b1e7f75bc07c2a51c1530483ea8d8
Ali Lutfi <ali.lutfi@mynbps.org>
2020-12-31 23:25:39 +0000

    Rather than using an "Array List," I used an array, which can not be resized, to contain all of the player's items in their inventory. The problem with using an array is that they can't be resized, so you couldn't even add items to the player's inventory once they found something ( say they found bronze, or diamond, but they couldn't pick it up) To fix this I imported the ArrayList class and then use an arraylist to create the player's inventory in the miner class. This is the most important thing I've done so far, and I had to remove many lines of code in the miner class because they all pertained to an array, rather than an arraylist, where the latter is what we need.

--- App.java
+++ App.java
@@ -1,15 +1,12 @@
-import java.util.*;
+import java.util.*; // we will be using scanner objects, so this is important to import.
 
 public class App {
-  
-
-     // will be using later, shows random materials miners can acquire while mining
-
     public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
       while(true){ // Everything is in THIS while loop
         // start of first nested while loop
-        while(True){ // this while loop will pretty much start the game, ask the player to say yes or no, kinda unimportant but adds to sophistication      
+        while(true){ // this while loop will pretty much start the game, ask the player to say yes or no, adds to sophistication      
+          Miner MinerObj = new Miner(100, 0.0, 0);    // create our "Miner" object, which is essentially the player.
           System.out.println("Welcome to the game! You'll be embarking on a mining mission.");
           System.out.println("To win, you need to not only acquire a net worth of fifteen dollars, but you also need 200 pounds ( in weight ) worth of items to win.");
           System.out.println("Sounds hard? Well, that's life. Let's get started!");
@@ -17,7 +14,7 @@ public class App {
           System.out.println("Greetings, fellow miner.");
           System.out.println("Would you like to hop in my brand new 'Dirt-Stone Digging Ship' with me and go on a little..");
           System.out.println("Mining trip!?!?!");
-          while(True){ // nested while loop, will keep on repeating until the player says "Yes". they don't have any other option because its pretty obvious. comedical (kinda)
+          while(true){ // nested while loop, will keep on repeating until the player says "Yes". they don't have any other option because its pretty obvious. comedical (kinda)
             System.out.println("(For the essentiality of the game, type 'Yes' with a capitalized Y, or 'No' with a capitalized N, depending on your interests :D ) ");
             String answer = input.nextLine();
             if (answer.equals("Yes")){
@@ -35,9 +32,6 @@ public class App {
     // end of first while loop
     // __________________________
     // start of second nested while loop
-      while(True){
-
-      }
 
       }
 }
--- Miner.java
+++ Miner.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
+
 public class Miner{
   double wealth;
   int health;
   int weightInPounds;
-  String[] materials = {"Bronze", "Bronze", "Bronze", "Bronze", "Silver", "Silver", "Silver", "Gold", "Gold", "Diamond"};
-  String[] inventory = {};
+  ArrayList<String> playerInventory = new ArrayList<String>();
   
-  public Player(int playerHealth, double playerWealth, int materialWeight){
+  public Miner(int playerHealth, double playerWealth, int materialWeight){
     health = playerHealth;
     wealth = playerWealth;
     weightInPounds = materialWeight;
@@ -17,49 +18,11 @@ public class Miner{
   }
 
  
-  public void addMaterialAndIncreaseWealth(){
-      int randomNum = (int)(Math.random() * 10);
-      randMaterial = materials[randomNum];
-      inventory.add(randMaterial);
-      if (randMaterial.equals(materials[0]) || randMaterial.equals(materials[1]) || randMaterial.equals(materials[2]) || randMaterial.equals(materials[3])){
-        wealth += 1.25;
-      }
-      else if (randMaterial.equals(materials[4]) || randMaterial.equals(materials[5]) || randMaterial.equals(materials[6])){
-        wealth += 1.50;
-      }
-      else if (randMaterial.equals(materials[7]) || randMaterial.equals(materials[8])){
-        wealth += 1.75;
-      }
-      else {
-        wealth += 2.00;
-      }
-      System.out.println("You have acquired: " + randMaterial);
-      System.out.println("Your current net worth is: " + wealth);
-  }
-
+  
   public boolean checkRequirementsToWin(){
-      for(int i = 0; i < inventory.length; i++){
-          if (inventory[i].equals("Bronze")){
-            weightInPounds += 5;
-          }
-          else if (inventory[i].equals("Silver")){
-            weightInPounds += 10;
-          }
-          else if (inventory[i].equals("Silver")){
-            weightInPounds += 15;
-          }
-          else {
-            weightInPounds += 25;
-          }
-  }
-    if (weightInPounds >= 200 && wealth >= 15 ){
-        System.out.println("You've met the requirements to win! Nice!");
-        return true;
-    }
-    else {
-      return false;
-    }
+    return true;
 }
+
 }
 
 //Thread.sleep(4000);
