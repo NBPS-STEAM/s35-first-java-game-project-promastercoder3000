67dd53948c2e26eb70d85fa1a7d8907ebf474f22
Ali Lutfi <ali.lutfi@mynbps.org>
2021-01-01 22:12:32 +0000

    I finished the findRandMaterial methods and the printStats methods. Will finish the other two methods in a bit as well as add comments to explain code.

--- App.java
+++ App.java
@@ -6,33 +6,46 @@ public class App {
       while(true){ // Everything is in THIS while loop
         // start of first nested while loop
         while(true){ // this while loop will pretty much start the game, ask the player to say yes or no, adds to sophistication      
-          Miner MinerObj = new Miner(100, 0.0, 0);    // create our "Miner" object, which is the player.
-          System.out.println("Welcome to the game! You'll be embarking on a mining mission.");
-          System.out.println("To win, you need to not only acquire a net worth of fifteen dollars, but you also need 200 pounds ( in weight ) worth of items to win.");
-          System.out.println("Sounds hard? Well, that's life. Let's get started!");
+          Miner MinerObj = new Miner(100, 0.0, 0);
+          MinerObj.findRandMaterial();
+          MinerObj.printStats();   // create our "Miner" object, which is the player.
+          System.out.println("(Welcome to the game! You'll be embarking on a mining mission.)");
+          System.out.println("(To win, you need to not only acquire a net worth of fifteen dollars, but you also need 200 pounds ( in weight ) worth of items to win.)");
+          System.out.println("(Sounds hard? Well, that's life. Let's get started!)");
           System.out.println("(The game has begun, and miner Dawson meets you at the mining station.");
-          System.out.println("Greetings, fellow miner.");
-          System.out.println("Would you like to hop in my brand new 'Dirt-Stone Digging Ship' with me and go on a little..");
-          System.out.println("Mining trip!?!?!");
+          System.out.println("Miner Dawson: Greetings, fellow miner.");
+          System.out.println("Miner Dawson: We've built a brand-new 'Dirt-Stone Digging Machine' for you. It can speak and burrow through the Earth quicky and efficiently. Would you like to hop in and go on a.. ");
+          System.out.println("Miner Dawson: Mining trip!?!?!");
           while(true){ // nested while loop, will keep on repeating until the player says "Yes". they don't have any other option because its pretty obvious. comedical (kinda)
-            System.out.println("(For the essentiality of the game, type 'Yes' with a capitalized Y, or 'No' with a capitalized N, depending on your interests :D ) ");
+            System.out.println("(For the essentiality of the game, type 'Yes' with a capitalized Y, or 'No' with a capitalized N.) ");
             String answer = input.nextLine();
             if (answer.equals("Yes")){
               System.out.println("Alright, lets go!");
               break;
             }
             else if (answer.equals("No")){
-              System.out.println("That's cool and all, but we're gonna ask again.");
+              System.out.println("That's cool and all, but I'm gonna ask again.");
             }
             else {
               System.out.println("You must have made a spelling mistake or forgot to capitalize something. Make sure you typed 'Y-e-s' or 'N-o' with capitalized first letters.");
             }
           }
+          System.out.println("Step into the ship, and go from there.");
+          System.out.println("(The ship's doors open, and you take a step into the tight, although oddy comfortable, space, and sit in a black, leather chair in front of a glass window.");
+          System.out.println("(The ship's door closes, and the machine begins speaking to you.");
+          System.out.println("Machine: Hello. I have not been given a name yet, but you can call me Machine. ");
+          System.out.println("Machine> I've been programmed by Mr. Dawson to take you on a little adventure. I've been ordered not to take you back up to the station until we get $15 as well as 200 pounds of materials. We will venture in three. two.. one.. ");
+          System.out.println("(A hatch in the stadium's floor opens, and the ship begins digging through aggressively.)");
+          System.out.println("Machine: Dawson did not mention this, but there are dangerous, sneaky moles inside the dirt. Please be careful, because I don't want to get damaged, for us machines have feelings too. I also was not programmed to say these exact words.");
+          System.out.println("(Dirt and soil stain your window as you make your way through underground tunnels.)");
+          break;
       }
     // end of first nested while loop
     // __________________________
     // start of second nested while loop
-
-      }
+        while(true){
+          ;
+        }
+      }       
 }
 }
--- Miner.java
+++ Miner.java
@@ -8,19 +8,34 @@ public class Miner{
   double wealth; // variable to represnt player's wealth
   int health; //player's health when fighting the moles ( underground animals)
   int weightInPounds; //weight of materials
-  String[] materials = {"Bronze", "Silver", "Gold", "Diamond"}; //materials available in the game, each material has its own worth as well as weight in pounds..
+  String[] materials = {"Bronze", "Silver", "Gold", "Diamond", "Bronze", "Silver", "Gold", "Bronze", "Silver", "Bronze"}; //materials available in the game, each material has its own worth as well as weight in pounds..
+
+  // each string in the array is considered a "block" of that material type, rather than a "gram," kinda like minecraft, where you find blocks of these materials instead of individual materials.
+  
+  // the weight and price of each is not according
+  // to reality, rather, to the game, so you may find a discrepancy between the value and weight of these items compared to their real-life counterparts.
+
+  // bronze = $1, silver = $2, gold = $4, and diamond = $8. essentially the value is doubled by each material.
+  // bronze = 12 pounds, silver = 24 pounds, gold = 48 pounds, diamond = 96 pounds
+
   ArrayList<String> playerInventory = new ArrayList<String>();
   
-  public Miner(int playerHealth, double playerWealth, int materialWeight){    //here, we create the constructor for the class, setting our appropiate variables.
+  
+  public Miner(int playerHealth, double playerWealth, int materialWeight){    
+    //here, we create the constructor for the class, setting our appropiate variables.
     health = playerHealth;
     wealth = playerWealth;
     weightInPounds = materialWeight;
   }
 
-  public void takeDamage(){   // this method is for taking damage to the player, which is a side struggle of the game that the player must worry about.
+  
+  public void takeDamage(){ 
+      // this method is for taking damage to the player's machine, which is a side struggle of the game that the player must worry about.
+    System.out.println("Machine: Mole spotted! Ahh!");
     health = health - 10;
-    System.out.println("You have taken 10 damage. You are now at " + health + " health!"); //print out player's health after they're damaged.
+    System.out.println("Machine: The mole has clawed at me and drained 10% of my battery. I'm now at " + health + " health left!\n Please be careful, or we'll both be stuck and buried within the dirt of the Earth, unable to move!"); //print out player's health after they're damaged.
   }
+
 
   //method for finding bronze/silver/gold/diamond below
   // in this method, we'll have to :
@@ -33,7 +48,36 @@ public class Miner{
       int randItem = rand.nextInt(materials.length); // here, we create an integer variable called randItem that generates a random number from 0
       // to the length of the array. We will use this to capture a random index from the materials array.
       playerInventory.add(materials[randItem]);
+      if(materials[randItem].equals("Bronze")){
+        System.out.println("Machine: Bronze block located. $1 increase in net worth as well as a 12 pound increase in weight. Great.");
+        wealth += 1;
+        weightInPounds +=12;
+      }
+      else if(materials[randItem].equals("Silver")){
+        System.out.println("Machine: Silver block located! $2 increase in networth along with 24 pounds of silver added to your inventory.");
+        wealth += 2;
+        weightInPounds += 24;
+      }
+      else if(materials[randItem].equals("Gold")){
+        System.out.println("Machine: Gold block located! How rare! $4 increase in net worth and 48 pounds ");
+        wealth += 4;
+        weightInPounds += 48;
+      }
+      else if(materials[randItem].equals("Diamond")){
+        System.out.println("Machine: You've spotted a whole diamond! Geez, I'm a machine and even I can feel the joy of finding this. You have increased eight dollars in net worth, and that'll be an added 96 pounds to your container. Lucky us.");
+        wealth += 8;
+        weightInPounds += 96;
+
+      }
       
+  }
+
+  public void printStats(){
+    // here we will print out the object's arraylist, which represents the inventory of the player,
+    // as well as the variables wealth and weightInPounds, which represent the player's current statistics ( how much money they have and how much their inventory weighs)
+    System.out.println("Machine: Now, here is everything in our container. " + playerInventory);
+    System.out.println("Machine: Our net worth is now: " + wealth + " dollars");
+    System.out.println("Machine: The weight in pounds of our container is " + weightInPounds + " pounds");
   }
 
   public void takeDamageOrFindMaterial(){ // here we'll either take damage to the player or find a material for the player's inventory and increase the wealth
@@ -47,3 +91,8 @@ public class Miner{
 }
 
 //Thread.sleep(4000);
+
+//To win,
+ //you need:
+//a net worth of fifteen dollars, 
+// 200 pounds ( in weight ) worth of items to win.
