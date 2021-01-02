007413bab3220cc05edfda968c6d1bdba5a793cb
Ali Lutfi <ali.lutfi@mynbps.org>
2021-01-02 04:20:58 +0000

    Finished commenting out the method findRandMaterial(). Still have to finish two more methods, possibly three, and then all that is left is to move these methods to the main class, App.

--- Miner.java
+++ Miner.java
@@ -43,31 +43,37 @@ public class Miner{
   // add the random material to the arraylist
   // increase the wealth of the player depending on material found
   // add to weight of pounds depending on material found
+  // NOTICE: Each material has a different worth as well as different weight in pounds associated with the material. This is why there are different
+  // increases in the integer variables "wealth" and 'weightInPounds' depending on what each material is. 
+  // This is why we have incorporated if statements that check what material has been randomly generated and compares it to a certain string, as shown below.
+  // An if statement turns out to be true when the element indicated by the randomly generated index of the array is equal to the string it is being compared to.
+  // The program will go through each if statement until the material-specific if statement that returns true is found and executed.
+  // For purposes of abstraction, all these if statements are contained within the below method. Many lines of code have been simplified into a single method
+  // that we will call in the main class, App.
   public void findRandMaterial(){ 
       Random rand = new Random(); // here we instantiate a random object that will later be used in our code.
       int randItem = rand.nextInt(materials.length); // here, we create an integer variable called randItem that generates a random number from 0
       // to the length of the array. We will use this to capture a random index from the materials array.
       playerInventory.add(materials[randItem]);
-      if(materials[randItem].equals("Bronze")){
-        System.out.println("Machine: Bronze block located. $1 increase in net worth as well as a 12 pound increase in weight. Great.");
-        wealth += 1;
-        weightInPounds +=12;
+      if(materials[randItem].equals("Bronze")){ // an if statement that checks if the element indicated by the randomly generated index of the array is equal to the string "Bronze" 
+        System.out.println("Machine: Bronze block located. $1 increase in net worth as well as a 12 pound increase in weight. Great."); // A message that is outputted each time this if statement is true (if the element is equal to "Bronze")
+        wealth += 1; // increase the value of the integer variable wealth by 1 if the if statement is true
+        weightInPounds += 12; // increase the value of the integer variable weightInPounds by 12 if the if statement is true
       }
-      else if(materials[randItem].equals("Silver")){
-        System.out.println("Machine: Silver block located! $2 increase in networth along with 24 pounds of silver added to your inventory.");
-        wealth += 2;
-        weightInPounds += 24;
+      else if (materials[randItem].equals("Silver")){  // an if statement that checks if the element indicated by the randomly generated index of the array is equal to the string "Silver"
+        System.out.println("Machine: Silver block located! $2 increase in networth along with 24 pounds of silver added to your inventory."); // A message outputted if the if statement is true
+        wealth += 2; // increase the value of the integer variable wealth by 2 if the if statement is true
+        weightInPounds += 24; // increase the value of the integer variable weightInPounds by 24 if the if statement is true
       }
-      else if(materials[randItem].equals("Gold")){
-        System.out.println("Machine: Gold block located! How rare! $4 increase in net worth and 48 pounds ");
-        wealth += 4;
-        weightInPounds += 48;
+      else if(materials[randItem].equals("Gold")){ // an if statement that checks if the element indicated by the randomly generated index of the array is equal to the string "Gold"
+        System.out.println("Machine: Gold block located! How rare! $4 increase in net worth and 48 pounds "); // A message outputted if the if statement is true
+        wealth += 4; // increase the value of the integer variable wealth by 4 if the if statement is true
+        weightInPounds += 48; // increase the value of the integer variable weightInPounds by 48 if the if statement is true
       }
-      else if(materials[randItem].equals("Diamond")){
-        System.out.println("Machine: You've spotted a whole diamond! Geez, I'm a machine and even I can feel the joy of finding this. You have increased eight dollars in net worth, and that'll be an added 96 pounds to your container. Lucky us.");
-        wealth += 8;
-        weightInPounds += 96;
-
+      else if(materials[randItem].equals("Diamond")){ // an if statement that checks if the element indicated by the randomly generated index of the array is equal to the string "Gold"
+        System.out.println("Machine: You've spotted a whole diamond! Geez, I'm a machine and even I can feel the joy of finding this. You have increased eight dollars in net worth, and that'll be an added 96 pounds to your container. Lucky us."); // A message outputted if the if statement is true 
+        wealth += 8; // increase the value of the integer variable wealth by 8 if the if statement is true
+        weightInPounds += 96; // increase the value of the integer variable weightInPounds by 96 if the if statement is true.
       }
       
   }
@@ -80,7 +86,8 @@ public class Miner{
     System.out.println("Machine: The weight in pounds of our container is " + weightInPounds + " pounds");
   }
 
-  public void takeDamageOrFindMaterial(){ // here we'll either take damage to the player or find a material for the player's inventory and increase the wealth
+  public void takeDamageOrFindMaterial()
+  { // here we'll either take damage to the player or find a material for the player's inventory and increase the wealth
   ;    
   }
   
